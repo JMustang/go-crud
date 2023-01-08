16be7eb81c23846c930ed4da5ec01a7b9a26fff1
Junior <eecfredes@gmail.com>
2023-01-08 20:04:35 -0300

    :sparkles: feat: configs - base configs

---
+++ configs/config.go
@@ -0,0 +1,20 @@
+package configs
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Pass     string
+	Database string
+}
