d35ca60d1862322f880fdcf482b26f1d00a69cc5
Junior <eecfredes@gmail.com>
2023-01-09 20:39:55 -0300

    :sparkles: feat: configs - function Load

--- configs/config.go
+++ configs/config.go
@@ -26,3 +26,30 @@ func init() {
 	viper.SetDefault("database.host", "localhost")
 	viper.SetDefault("database.poet", "5432")
 }
+
+func Load() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cfg = new(config)
+
+	cfg.API = APIConfig{
+		Port: viper.GetString("api.port"),
+	}
+
+	cfg.DB = DBConfig{
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetString("database.port"),
+		User:     viper.GetString("database.user"),
+		Pass:     viper.GetString("database.pass"),
+		Database: viper.GetString("database.name"),
+	}
+	return nil
+}
