8e77535e45c5818c5541074c88e86c21e106b108
Junior <eecfredes@gmail.com>
2023-01-15 14:40:10 -0300

    :sparkles: feat: handles - list

---
+++ handles/list.go
@@ -0,0 +1,19 @@
+package handles
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/JMustang/go-crud/models"
+)
+
+func List(w http.ResponseWriter, r *http.Request) {
+	todos, err := models.GetAll()
+	if err != nil {
+		log.Printf("Erro ao obter registros: %v", err)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
